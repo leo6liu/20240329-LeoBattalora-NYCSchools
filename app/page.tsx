c3bba6816c66c10c95020b51b98255d2c60f18ff
Leo <leo.battalora@gmail.com>
2024-04-01 00:17:10 -0400

    edit home text

--- app/page.tsx
+++ app/page.tsx
@@ -7,30 +7,34 @@ export default function Home() {
   return (
     <>
       {/* page */}
-      <div className="mx-auto flex w-full max-w-screen-sm flex-col space-y-4 py-12 px-4 text-primary dark:text-secondary-focus-dark font-sans">
+      <div className="mx-auto flex w-full max-w-screen-sm flex-col space-y-4 px-4 pt-12 font-sans text-primary dark:text-secondary-focus-dark">
         <h1 className="pb-3 text-4xl font-semibold text-primary dark:text-primary-dark">
           What is this?
         </h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          This site is a sample project to demonstrate some of my UI/UX skills
+          for a job application. It was created over the course of one long
+          afternoon on 2024/03/31. It is a simple webapp that calculates the
+          average SAT scores for the five boroughs of NYC.
         </p>
-
-        <div className="w-[32rem] ">
+        <div className="mx-auto w-[24rem] py-8 sm:w-[30rem]">
           <NYC
             colors={{
-              "otherLand": "rgb(39 39 42)",
-              "water": "none",
-              "bronx": "rgb(244 244 245)",
-              "brooklyn": "rgb(82 82 91)",
-              "manhattan": "rgb(212 212 216)",
-              "queens": "rgb(113 113 122)",
-              "statenIsland": "rgb(161 161 170)",
+              otherLand: "rgb(39 39 42)",
+              water: "none",
+              bronx: "rgb(244 244 245)",
+              brooklyn: "rgb(82 82 91)",
+              manhattan: "rgb(212 212 216)",
+              queens: "rgb(113 113 122)",
+              statenIsland: "rgb(161 161 170)",
             }}
           />
         </div>
+        <p>
+          Try the website out in dark mode by clicking the moon icon in the
+          navigation bar. And also explore how the site responds to different
+          screen sizes!
+        </p>
       </div>
     </>
   );
